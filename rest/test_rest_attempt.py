91b610642d8dc12412499e65a3e76c644accd14e
testify <alesvlcek@gmail.com>
2023-04-27 11:32:59 +0200

    added assertpy to have better assertions

--- rest/test_rest_attempt.py
+++ rest/test_rest_attempt.py
@@ -1,12 +1,14 @@
 import logging
 
 import requests
+from assertpy import assert_that
 
 
 def test_rest_attempt():
     response = requests.get('https://api.github.com/events')
 
     logging.getLogger().info("==============" + str(response.status_code) + "==============")
+    assert_that(response.status_code).is_equal_to(200)
     # logging.getLogger().info("==============" + str(response.json()) + "==============")
     logging.getLogger().info("==============" + str(response.headers) + "==============")
     logging.getLogger().info("==============" + str(response.headers["Content-Type"]) + "==============")
